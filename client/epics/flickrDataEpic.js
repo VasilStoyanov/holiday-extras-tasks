4d25bea5f6a5f5be5743cb46d602b4a23e39e2d6
VasilStoyanov <vasil.stoyanov94@gmail.com>
2018-05-22 13:49:11 +0300

    Fixed bug with creation of new users

--- client/epics/flickrDataEpic.js
+++ client/epics/flickrDataEpic.js
@@ -4,24 +4,21 @@ import * as axios from 'axios';
 import jQuery from 'jquery';
 
 const DEBOUNCE_TIME_IN_MS = 200;
-const flickrApiPoint = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+const flickrApiPoint = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
 
 const getFlickerData = () => new Promise((resolve, reject) => {
   jQuery.ajax({
     url: flickrApiPoint,
     dataType: 'jsonp',
-    data: { format: "json" },
+    data: { format: 'json' },
     success: (data) => {
-      console.log('here');
-      console.log(data);
       resolve(data);
     },
     error: (error) => {
       reject(error);
-    }
+    },
   });
 });
-
 
 export const fetchFlickrData = action$ =>
   action$.ofType('FETCH_FLICKR_DATA')
--- client/epics/userEpic.js
+++ client/epics/userEpic.js
@@ -1,5 +1,6 @@
-import { map, pluck, switchMap, debounceTime } from 'rxjs/operators';
+import { filter, map, pluck, switchMap, debounceTime, catchError, tap } from 'rxjs/operators';
 import { fromPromise } from 'rxjs/observable/fromPromise';
+import { of } from 'rxjs/observable/of';
 import * as axios from 'axios';
 
 const USER_FETCH_DEBOUNCE_TIME_IN_MS = 200;
@@ -8,7 +9,7 @@ export const fetchUsersEpic = action$ =>
   action$.ofType('FETCH_ALL_USERS')
     .pipe(
       debounceTime(USER_FETCH_DEBOUNCE_TIME_IN_MS),
-      switchMap(() => fromPromise(axios.get('http://localhost:3001/api/user'))),
+      switchMap(() => fromPromise(axios.get('http://localhost:3000/api/user'))),
       pluck('data'),
       map(users => ({ type: 'FETCH_ALL_USERS_COMPLETED', payload: users })),
     );
@@ -19,7 +20,11 @@ export const createNewUserEpic = action$ =>
       switchMap(action => fromPromise(axios.post(
         'http://localhost:3000/api/user/create',
         action.payload,
-      ))),
+      ))
+        .pipe(
+          catchError(() => of(Object.create(null))),
+          filter(obj => obj.data),
+        )),
       pluck('data'),
       map(user => ({ type: 'CREATE_NEW_USER_SUCCESS', payload: user })),
     );
